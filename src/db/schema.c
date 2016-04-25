3125ff31b705abea6e42ad11eec9a773aadfc5e2
Frank W. Miller <frmi5636@colorado.edu>
2016-04-24 19:13:34 -0700

    Schema updates

---
+++ src/db/schema.c
@@ -0,0 +1,31 @@
+#include "schema.h"
+
+void
+schema_element_alloc(element_t elem, enum base_types bt)
+{
+
+}
+
+void
+schema_element_print(element_t elem)
+{
+
+}
+
+void
+schema_element_insert(schema_t s, element_t elem)
+{
+
+}
+
+void
+schema_element_remove(schema_t s, element_t elem)
+{
+
+}
+
+void
+schema_print(schema_t s)
+{
+
+}
