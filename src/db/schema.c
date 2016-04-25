edc75f8c32f35fdf912498bb9dc65a11b3cd0119
Frank W. Miller <frmi5636@colorado.edu>
2016-04-24 19:39:01 -0700

    Added to schema_element_print()

--- src/db/schema.c
+++ src/db/schema.c
@@ -1,3 +1,6 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "schema.h"
 
 void
@@ -9,7 +12,33 @@ schema_element_alloc(element_t elem, enum base_types bt)
 void
 schema_element_print(element_t elem)
 {
+	assert (elem != NULL);
 
+	switch (elem->bt) {
+	case CHARACTER:
+		break;
+	case VARCHAR:
+		printf("VARCHAR");
+		break;
+	case BOOLEAN:
+		printf("BOOL");
+		break;
+	case INTEGER:
+		printf("INT");
+		break;
+	case FLOAT:
+		printf("FLOAT");
+		break;
+	case DOUBLE:
+		printf("DOUBLE");
+		break;
+	case DATE:
+		printf("DATE");
+		break;
+	case TIME:
+		printf("TIME");
+		break;
+	}
 }
 
 void
