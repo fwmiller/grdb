a444f13231f36d1f9fcca2543fc20ea079457ade
Frank W. Miller <frmi5636@colorado.edu>
2016-10-02 22:42:23 -0700

    More string pool testing

--- src/cli/main.c
+++ src/cli/main.c
@@ -20,6 +20,10 @@ main(int argc, char**argv)
 	string_pool_insert(&pool, "Another test");
 	string_pool_print(pool);
 
+	printf("\ninsert \"Yet another\"\n");
+	string_pool_insert(&pool, "Yet another");
+	string_pool_print(pool);
+
 	cli();
 	exit(0);
 }
--- src/db/string_pool.c
+++ src/db/string_pool.c
@@ -22,7 +22,7 @@ string_pool_init(string_pool_t *pool)
 void
 string_pool_print(string_pool_t pool)
 {
-	int i, n, pos;
+	int i, n;
 
 	if (pool == NULL) {
 #if _DEBUG
@@ -39,10 +39,14 @@ string_pool_print(string_pool_t pool)
 			*((unsigned short *) (pool + 1)));
 	printf("\n");
 
-	for (pos = 0, i = 0;
-	     i < n;
-	     pos += ((unsigned short *) (pool + 3))[i], i++)
-		printf("[%s]\n", pool + 3 + n * 2 + pos);
+	for (i = 0; i < n; i++) {
+		unsigned short idx;
+		unsigned char *pos;
+
+		idx = *((unsigned short *) (pool + 3 + i * 2));
+		pos = pool + 3 + n * 2;
+		printf("[%s]\n", pos + idx);
+	}
 }
 
 void
@@ -62,11 +66,11 @@ string_pool_insert(string_pool_t *pool, char *s)
 	n = (*pool)[0];
 
 	/* plen is the length of the current string pool memory */
-	plen = (n > 0 ? *((unsigned short *) (pool + 1)) : 0);
+	plen = (n > 0 ? *((unsigned short *) ((*pool) + 1)) : 0);
 
 #if _DEBUG
 	printf("entries %d string pool memory length %d bytes\n", n, plen);
-	bufdump((char *) (pool + 3 + n * 2), plen);
+	bufdump((char *) ((*pool) + 3 + n * 2), plen);
 #endif
 	/* blen is the overall size of new string pool */
 	blen = 3 + ((n + 1) * 2) + plen + len + 1;
@@ -84,13 +88,13 @@ string_pool_insert(string_pool_t *pool, char *s)
 	*((unsigned short *) (buf + 1)) = plen + len + 1;
 
 	/* Copy array of indexes from old to new pool */
-	memcpy(buf + 3, pool + 3, n * 2);
+	memcpy(buf + 3, (*pool) + 3, n * 2);
 
 	/* Setup index pointer for new string as last index in new pool */
 	*((unsigned short *) (buf + 3 + n * 2)) = plen;
 
 	/* Copy string pool memory from old to new pool */
-	memcpy(buf + 3 + (n + 1) * 2, pool + 3 + n * 2, plen);
+	memcpy(buf + 3 + (n + 1) * 2, (*pool) + 3 + n * 2, plen);
 
 	/* Copy new string to end of string pool memory */
 	memcpy(buf + 3 + (n + 1) * 2 + plen, s, len);
