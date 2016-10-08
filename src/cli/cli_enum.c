7d526e6fc7006dbe1f43f32a3ba5874eae08f964
Frank W. Miller <frmi5636@colorado.edu>
2016-10-08 07:32:44 -0700

    Starting adding enum command to cli

---
+++ src/cli/cli_enum.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdlib.h>
+#include "cli.h"
+#include "enum.h"
+
+void
+cli_enum(char *cmdline, int *pos)
+{
+	char s[BUFSIZE];
+
+	assert(cmdline != NULL);
+	assert(pos != NULL);
+}
