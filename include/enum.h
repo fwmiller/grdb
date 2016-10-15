af4b1dc115ce534c0702f4732711affb7c4e4c61
Frank W. Miller <frmi5636@colorado.edu>
2016-10-15 08:36:42 -0700

    Add enum linked lists

--- include/enum.h
+++ include/enum.h
@@ -1,12 +1,22 @@
-#ifndef __ENUMERATIO_H
-#define __ENUMERATIO_H
+#ifndef __ENUMERATION_H
+#define __ENUMERATION_H
 
 #include "string_pool.h"
 
-typedef string_pool_t enum_t;
+struct grdb_enum {
+	string_pool_t pool;
+	struct grdb_enum *next;
+};
+
+typedef struct grdb_enum *enum_t;
+typedef enum_t enum_list_t;
 
 void enum_init(enum_t *e);
 void enum_print(enum_t e);
 void enum_insert(enum_t *e, char *s);
+
+void enum_list_init(enum_list_t *el);
+void enum_list_print(enum_list_t el);
+void enum_list_insert(enum_list_t *el, enum_t e);
 
 #endif
--- src/db/enum.c
+++ src/db/enum.c
@@ -1,20 +1,64 @@
+#include <assert.h>
+#include <stdlib.h>
 #include "enum.h"
 
 
 void
 enum_init(enum_t *e)
 {
-	string_pool_init(e);
+	assert (e != NULL);
+
+	if (*e != NULL)
+		free(*e);
+
+	*e = malloc(sizeof(struct grdb_enum));
+	assert(*e != NULL);
+
+	string_pool_init(&((*e)->pool));
+	(*e)->next = NULL;
 }
 
 void
 enum_print(enum_t e)
 {
-	string_pool_print(e);
+	string_pool_print(e->pool);
 }
 
 void
 enum_insert(enum_t *e, char *s)
 {
-	string_pool_insert(e, s);
+	assert (e != NULL);
+	assert (*e != NULL);
+	assert (s != NULL);
+
+	string_pool_insert(&((*e)->pool), s);
+}
+
+void
+enum_list_init(enum_list_t *el)
+{
+	assert (el != NULL);
+	*el = NULL;
+}
+
+void
+enum_list_print(enum_list_t el)
+{
+	enum_t e;
+
+	assert (el != NULL);
+
+	for (e = el; e != NULL; e = e->next)
+		enum_print(e);
+}
+
+void
+enum_list_insert(enum_list_t *el, enum_t e)
+{
+	assert (el != NULL);
+	assert (e != NULL);
+
+	if (*el != NULL)
+		e->next = *el;
+	*el = e;
 }
