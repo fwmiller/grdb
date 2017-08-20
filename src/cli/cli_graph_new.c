6741839865884d5270d9e9ea7ab5c49dd878d54e
Frank W. Miller <frmi5636@colorado.edu>
2017-08-20 08:55:22 -0700

    cli_graph_new() working

--- src/cli/cli_graph_new.c
+++ src/cli/cli_graph_new.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <dirent.h>
 #if _DEBUG
 #include <errno.h>
 #endif
@@ -8,13 +10,61 @@
 #include <unistd.h>
 #include "cli.h"
 
+static int
+numbers_only(const char *s)
+{
+	while (*s) {
+		if (isdigit(*s++) == 0)
+			return 0;
+	}
+	return 1;
+}
+
+static int
+cli_graph_next_gno()
+{
+	DIR *dirfd;
+	struct dirent *de;
+	int n = gno;
+	int i;
+
+	/* Search the grdb directory for the highest gno */
+	if ((dirfd = opendir(grdbdir)) == NULL)
+		return (-1);
+
+	for (;;) {
+		de = readdir(dirfd);
+		if (de == NULL)
+			break;
+
+		if (!numbers_only(de->d_name))
+			continue;
+
+		i = atoi(de->d_name);
+		if (i > n)
+			i = n;
+	}
+	closedir(dirfd);
+#if _DEBUG
+	printf("cli_graph_next_gno: next gno %d\n", n + 1);
+#endif
+	return n + 1;
+}
+
 void
 cli_graph_new(char *cmdline, int *pos)
 {
 	struct vertex v;
 	char s[BUFSIZE];
-	int fd;
+	int fd, n;
 
+	n = cli_graph_next_gno();
+	if (n < 0) {
+#if _DEBUG
+		printf("cli_graph_new: bad next gno\n");
+#endif
+		return;
+	}
 	/* Initialize global graph and component numbers if necessary */
 	if (gno < 0 && cno < 0) {
 		gno = 0;
@@ -26,16 +76,16 @@ cli_graph_new(char *cmdline, int *pos)
 
 	/* Persistence... */
 	memset(s, 0, BUFSIZE);
-	sprintf(s, "%s/%d", grdbdir, gno);
+	sprintf(s, "%s/%d", grdbdir, n);
 	mkdir(s, 0755);
 
 	memset(s, 0, BUFSIZE);
-	sprintf(s, "%s/%d/%d", grdbdir, gno, cno);
+	sprintf(s, "%s/%d/%d", grdbdir, n, 0);
 	mkdir(s, 0755);
 
 	/* Create component vertex file */
 	memset(s, 0, BUFSIZE);
-	sprintf(s, "%s/%d/%d/v", grdbdir, gno, cno);
+	sprintf(s, "%s/%d/%d/v", grdbdir, n, 0);
 #if _DEBUG
 	printf("cli_graph_new: open vertex file %s\n", s);
 #endif
