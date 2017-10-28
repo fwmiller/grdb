04171e9a839856a22444ead342c9076880381cdf
Frank W. Miller <frmi5636@colorado.edu>
2017-10-28 10:18:08 -0700

    Sketching in component_join.c

--- src/db/component_join.c
+++ src/db/component_join.c
@@ -1,5 +1,27 @@
 #include "graph.h"
 
+/*
+ * This function combines two component structures only.  It does nothing
+ * with the enums or schemas.
+ */
+static component_t
+component_join_graphs(component_t c1, component_t c2)
+{
+	return NULL;
+}
+
+
+/* This function combines two enums */
+
+
+/* This function combines two schemas */
+
+
+/*
+ * This function does two things.  1) It builds a new component structure
+ * based on the structures of the two input components and 2) It builds
+ * the enums and vertex and edge schemas from the two input components.
+ */
 component_t
 component_join(component_t c1, component_t c2)
 {
