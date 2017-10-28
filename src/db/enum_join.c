b305d27298c0ebef7de7ec5910448ed834c63442
Frank W. Miller <frank@frankwmiller.net>
2017-10-28 11:08:25 -0700

    Add enum_join() and schema_join()

---
+++ src/db/enum_join.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include "enum.h"
+
+enum_list_t
+enum_list_join(enum_list_t el1, enum_list_t el2)
+{
+	return NULL;
+}
+
---
+++ src/db/schema_join.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "schema.h"
+
+schema_t
+schema_join(schema_t s1, schema_t s2)
+{
+	return NULL;
+}
